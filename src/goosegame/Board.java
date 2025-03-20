dd603434cff8ecfde53d8f26f8bb123b771734a3
Yousra0225 <chbibyousra02@gmail.com>
2025-03-20 14:43:51 +0100

    feat(board): add board class

---
+++ src/goosegame/Board.java
@@ -0,0 +1,19 @@
+package goosegame;
+
+/**
+  * Class for Board
+  */
+public abstract class Board {
+  protected final int NB_OF_CELLS;
+  protected Cell[] theCells;
+
+  /**
+  * Constructor of Board abstract class
+  * @param nbOfCells number of cells in this board
+  */
+  public Board(int nbOfCells) {
+    this.NB_OF_CELLS = nbOfCells;
+    this.theCells = new Cell[nbOfCells + 1];
+    this.theCells[0] = new StartCell();
+    this.initBoard();
+  }
