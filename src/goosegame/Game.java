9db3ea5733dda76d31638f4eb0896da4fb2da078
Yousra0225 <chbibyousra02@gmail.com>
2025-03-20 14:42:09 +0100

    feat(strategy) : add game class

---
+++ src/goosegame/Game.java
@@ -0,0 +1,20 @@
+package goosegame;
+import java.util.*;
+/**
+ * Class for game
+ */
+public class Game {
+    protected List<Player> thePlayers;
+    protected Board board;
+    protected Iterator<Player> it;
+    /**
+     * Constructor of Game class
+     * @param b
+     */
+    public Game(Board b){
+        this.board = b;
+        this.thePlayers = new ArrayList<>();
+        this.it = thePlayers.iterator();
+    }
+
+}
