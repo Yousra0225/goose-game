26aecad385ab63a8ed6f4dfa7da7ebdf5d75c28a
Yousra0225 <chbibyousra02@gmail.com>
2025-03-24 12:41:26 +0100

    feat(board): add getNbOfCells method to retrieve the number of cells

--- src/goosegame/Board.java
+++ src/goosegame/Board.java
@@ -23,4 +23,22 @@ public abstract class Board {
   */
   abstract protected void initBoard();
 
+  /**
+  * Get the cell of index i in theCells
+  * @param i the index of the cell
+  * @return the cell of index i
+  */
+  public Cell getCell(int index) {
+    return this.theCells[index];
+  }
+
+  /**
+  * Get the number of cells in theCells
+  * @return the number of cells
+  */
+  public int getNbOfCells() {
+    return this.NB_OF_CELLS;
+  }
+
+
 }
