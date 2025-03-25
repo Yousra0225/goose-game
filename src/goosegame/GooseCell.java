c5b8b5e4e5ffb47cbd36d8e6111b009bbf03c3cd
Yousra0225 <chbibyousra02@gmail.com>
2025-03-25 23:12:33 +0100

    feat(GooseCell): add class with constructor

---
+++ src/goosegame/GooseCell.java
@@ -0,0 +1,16 @@
+package goosegame;
+
+/**
+  * Class for GooseCell
+  */
+public class GooseCell extends NormalCell {
+
+    /**
+     * Constructor of GooseCell class
+    * @param i index number of this cell
+    */
+    public GooseCell(int i) {
+        super(i);
+    }
+
+    
