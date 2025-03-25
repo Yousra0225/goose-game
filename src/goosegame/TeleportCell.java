fd8fe7e83381473e5af79c0910577abcaa37c8e5
Yousra0225 <chbibyousra02@gmail.com>
2025-03-25 22:54:16 +0100

    feat(TeleportCell): add type and waiting time methods

--- src/goosegame/TeleportCell.java
+++ src/goosegame/TeleportCell.java
@@ -15,5 +15,29 @@ public class TeleportCell extends NormalCell {
     super(initial);
     this.dest = destination;
   }
-  
+
+  /**
+    * Get the index of the final cell
+    * @param dt the value of the roll of the dice
+    * @return the index of the final cell
+    */
+    public int getFinalCell(int dt){
+        return this.dest;
+      }
+    
+    /**
+    * Get the type of this cell
+    * @return the type of this cell
+    */
+    public String getType(){
+        return "TeleportCell";
+    }
+
+    /**
+    * Get the nb of round to wait for
+    * @return the string about how much round to wait for
+    */
+    public String getWaitingTime(){
+        return "";
+    }
 }
