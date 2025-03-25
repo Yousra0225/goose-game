9455519b6fc0c49e87ba40e2af245123553255ef
Yousra0225 <chbibyousra02@gmail.com>
2025-03-25 23:34:15 +0100

    feat(game): Add game mechanics and player management
    - Implement getBoard() to retrieve the game board.
    - Add addPlayer(Player p) to register a player and place them on the start cell.
    - Implement isFinished() to check if the game has ended.
    - Implement nextPlayer() to get the next player in turn order.

--- src/goosegame/Game.java
+++ src/goosegame/Game.java
@@ -17,4 +17,42 @@ public class Game {
         this.it = thePlayers.iterator();
     }   
 
+    /**
+    * Get the board of this game
+    * @return the board
+    */
+    public Board getBoard() {
+        return this.board;
+      }
+  
+    /**
+    * Add a player in this game
+    * @param p player we want to add in this game
+    */
+    public void addPlayer(Player p){
+        this.thePlayers.add(p);
+        Cell cell = new StartCell();
+        p.changeCell(cell);
+        cell.welcomePlayer(p);
+    }
+  
+    /**
+    * know if the game is finished
+    * @return true if the game is finish otherwize false
+    */
+    public boolean isFinished() {
+        return this.board.getCell(this.board.getNbOfCells()).isOccupied();
+    }
+  
+  /**
+    * Get the next player who has to play
+    * @return the next Player
+    */
+    public Player nextPlayer(){
+        if( !this.it.hasNext() )
+        this.it = this.thePlayers.iterator();
+        return this.it.next();
+    }
+  
+
 }
