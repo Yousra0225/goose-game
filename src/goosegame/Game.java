7799c114afcfc2643d48d33844b05682840b9ddc
Yousra0225 <chbibyousra02@gmail.com>
2025-03-25 23:44:01 +0100

    feat: Implement player movement with cell validation
    - Add movePlayer method to handle player movement across cells.
    - Validate the destination cell index and adjust if necessary using caseValid.
    - Handle cases where the destination cell is occupied and notify the player.
    - Print messages for different cell types (Normal, Goose, Teleport) and actions.

--- src/goosegame/Game.java
+++ src/goosegame/Game.java
@@ -68,4 +68,39 @@ public class Game {
         }
         return res;
     }
+
+/** Move the player to one cell
+* @param player the player who want to move
+* @param valeur the value on the dice
+*/
+public void movePlayer(Player joueur, int valeur ){
+    Cell caseDepart = joueur.getCell();
+    int indiceCaseDepart = caseDepart.getIndex();
+
+    int indiceCaseArrive = this.caseValid(indiceCaseDepart + valeur) ;
+    Cell caseArrive = this.board.getCell(indiceCaseArrive);
+
+    int indiceCaseFinale = this.caseValid(caseArrive.getFinalCell(valeur));
+    Cell caseFinale = this.board.getCell(indiceCaseFinale);
+
+    String res = "";
+    if( caseFinale.isOccupied() ){
+        Player occupentPlayer = caseFinale.getPlayer();
+        caseDepart.welcomePlayer(occupentPlayer);
+        occupentPlayer.changeCell(caseDepart);
+        res = " cell is busy ! "+occupentPlayer.toString()+ " is sent to cell "+indiceCaseDepart;
+    }
+    else {
+        caseDepart.removePlayerAtCell();
+    }
+
+    if( caseArrive.getType() == "NormalCell" )
+        System.out.println(joueur.toString() + " is in cell " +indiceCaseDepart+ ", " +joueur.toString()+" throws " +valeur+" and reaches "+indiceCaseArrive+res);
+
+    if( caseArrive.getType() == "GooseCell" || caseArrive.getType() == "TeleportCell" )
+        System.out.println(joueur.toString() + " is in cell " +indiceCaseDepart+ ", " +joueur.toString()+" throws " +valeur+" and reaches "+indiceCaseArrive+" ("+caseArrive.getType()+") and jumps to cell "+indiceCaseFinale+" ("+caseArrive.getType()+")"+res);
+
+    caseFinale.welcomePlayer(joueur);
+    joueur.changeCell(caseFinale);
+}
 }
