bdc544855de955b7e6874bfb2933b9cb5628a140
Yousra0225 <chbibyousra02@gmail.com>
2025-03-25 23:06:25 +0100

    fix(WaitCell): adjust canBeLeft logic to decrement timeRemaining

--- src/goosegame/WaitCell.java
+++ src/goosegame/WaitCell.java
@@ -4,17 +4,30 @@ package goosegame;
   * Class for WaitCell
   */
 public class WaitCell extends NormalCell {
-  protected int timeToWait;
-  protected int timeRemaining;
+    
+    protected int timeToWait;
+    protected int timeRemaining;
 
-  /**
-  * Constructor of WaitCell class
-  * @param i index number of this cell
-  * @param timeWait time to wait to play
-  */
-  public WaitCell(int i, int timeWait) {
-    super(i);
-    this.timeToWait = timeWait;
-    this.timeRemaining = timeWait;
-  }
+    /**
+     * Constructor of WaitCell class
+    * @param i index number of this cell
+    * @param timeWait time to wait to play
+    */
+    public WaitCell(int i, int timeWait) {
+        super(i);
+        this.timeToWait = timeWait;
+        this.timeRemaining = timeWait;
+    }
+
+    /**
+    * Know if player can be left the cell
+    * @return true if player can play, false otherwize
+    */
+    public boolean canBeLeft(){
+        if(this.timeRemaining > 0){
+          this.timeRemaining -= 1;
+          return false;
+        }
+        return this.timeRemaining == 0;
+      }
 }
