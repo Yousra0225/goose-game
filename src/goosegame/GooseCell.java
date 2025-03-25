2297f24b228772cc6cfe3b8a62cd65fbcd0ee783
Yousra0225 <chbibyousra02@gmail.com>
2025-03-25 23:23:21 +0100

    feat(GooseCell): implement getFinalCell, getType, and getWaitingTime methods

--- src/goosegame/GooseCell.java
+++ src/goosegame/GooseCell.java
@@ -13,4 +13,30 @@ public class GooseCell extends NormalCell {
         super(i);
     }
 
-    
+    /**
+    * Get the index of the final cell
+    * @param dt the value of the roll of the dice
+    * @return the index of the final cell
+    */
+    public int getFinalCell(int dt){
+        return this.index + dt;
+    }
+
+    /**
+        * Get the type of this cell
+        * @return the type of this cell
+        */
+    public String getType(){
+        return "GooseCell";
+    }
+
+    /**
+        * Get the nb of round to wait for
+        * @return the string about how much round to wait for
+        */
+    public String getWaitingTime(){
+        return "";
+  }
+
+}
+ 
